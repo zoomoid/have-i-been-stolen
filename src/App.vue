738dc073e1bb19d86ed40e9a8f9cb8ff9d8e36c1
Alexander Bartolomey <occloxium@gmail.com>
2020-09-20 00:55:44 +0200

    :sparkles: add logo to main page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <v-app dark>
-    <v-app-bar flat class="primary" app>
-      <v-toolbar-title class="font-weight-bold">Have I Been Stolen?</v-toolbar-title>
-    </v-app-bar>
-    <v-content app class="primary">
+  <v-app>
+    <v-main>
+      <img src="/img/have-i-been-stolen-black.svg" alt="Have I Been Stolen?">
       <router-view></router-view>
-    </v-content>
+    </v-main>
   </v-app>
 </template>
+
+<style lang="sass" scoped>
+img
+  height: 128px
+  display: block
+  margin-left: 10%
+  margin-top: 10%
+</style>
